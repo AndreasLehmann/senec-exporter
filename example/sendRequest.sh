97466ffe21967df920fd8b97bb4360b494e833e0
Andreas Lehmann <andreas@andreaslehmann.de>
2021-12-04 10:26:26 +0100

    added PM1OBJ example response

--- example/sendRequest.sh
+++ example/sendRequest.sh
@@ -1,9 +1,10 @@
 #/bin/zsh
 
-DATA="{\"WIZARD\":{}}"
+#DATA="{\"WIZARD\":{}}"
 #DATA="{\"STATISTIC\":{}}"
 #DATA="{\"ENERGY\":{}}"
 #DATA="{\"PV1\":{}}"
+DATA="{\"PM1OBJ1\":{}}"
 
 IP=192.168.9.91
 
